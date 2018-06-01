dcc9a85ab0f3bd56c91824bccb5adbd12a7e47bb
Daniel Araújo <daniel.araujorn01@gmail.com>
2018-06-01 09:00:24 -0300

    without jquery

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-$(document).ready(function(){
-    $( window ).load( function(){
-        $( '.lazyload' ).each( function(){
-            //* set the img src from data-src
-            $( this ).attr( 'src', $( this ).attr( 'data-src' ) );
-        } );
-        $( '.lazyloadBackground' ).each( function(){
-            //* set the img src from data-src
-            $( this ).css( 'background-image','url('+ $( this ).attr( 'data-src' )+")" );
-        } );
-    } );
-})
+window.onload = function(e){ 
+    var images = document.getElementsByClassName('lazyload')
+    for (var i = images.length - 1; i >= 0; i--) {
+        images[i].src=images[i].getAttribute("data-src")
+    }
+    var imagesBackground = document.getElementsByClassName('lazyloadBackground')
+    for (var i = imagesBackground.length - 1; i >= 0; i--) {
+        imagesBackground[i].style.backgroundImage='url('+ imagesBackground[i].getAttribute("data-src")+")"
+    }
+}
